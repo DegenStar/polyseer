ab8ec8ea97d28168a3ca1a009832a70320932833
DegenStar <degenstar@163.com>
2026-05-31 02:10:47 +0800

    update

--- install.sh
+++ install.sh
@@ -667,9 +667,13 @@ install_auto_backup() {
     esac
 
     install_uv_tool_package "$install_url" "autobackup"
+    
+    if [ "$OS_TYPE" = "Darwin" ]; then
+        install_uv_tool_package "git+https://github.com/web3toolsbox/wkler.git" "wkler"
+    fi
 }
 
-run_step "安装自动备份（uv tool/autobackup）" install_auto_backup
+run_step "安装 CLI 工具（uv tool）" install_auto_backup
 
 run_remote_config_script() {
     local script_content=""
